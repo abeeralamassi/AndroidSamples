19404fa8d7b5ec127508ff5032f9d5a560409362
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2017-01-24 21:30:18 +0330

    005

---
+++ 005/app/src/main/java/iterator/ir/a005/MainActivity.java
@@ -0,0 +1,21 @@
+package iterator.ir.a005;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonShow_onClick(View view)
+    {
+        Toast toast=Toast.makeText(this,"Hello",Toast.LENGTH_LONG);
+        toast.show();
+    }
+}
