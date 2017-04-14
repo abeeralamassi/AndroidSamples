8f6416ec0a2b143a16d71929f5634e80c4b7f1bf
Mahmood Ramzani <ramzani.mahmood@gmail.com>
2017-04-14 15:11:24 +0430

    working on 062

--- 062/app/src/main/java/mhdr/ir/a062/SearchActivity.java
+++ 062/app/src/main/java/mhdr/ir/a062/SearchActivity.java
@@ -2,12 +2,39 @@ package mhdr.ir.a062;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SearchActivity extends AppCompatActivity {
+
+    Button buttonSearchContacts;
+    EditText editTextSearchName;
+    TextView textViewDisplayName;
+    TextView textViewDisplayPhone;
+    TextView textViewDisplayEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        buttonSearchContacts= (Button) findViewById(R.id.buttonSearchContacts);
+        buttonSearchContacts.setOnClickListener(buttonSearchContacts_OnClickListener);
+
+        editTextSearchName= (EditText) findViewById(R.id.editTextSearchName);
+        textViewDisplayName= (TextView) findViewById(R.id.textViewDisplayName);
+        textViewDisplayPhone= (TextView) findViewById(R.id.textViewDisplayPhone);
+        textViewDisplayEmail= (TextView) findViewById(R.id.textViewDisplayEmail);
     }
+
+    View.OnClickListener buttonSearchContacts_OnClickListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String nameToSearch=editTextSearchName.getText().toString();
+
+
+        }
+    };
 }
